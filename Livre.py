e2142d35e4e3bfd9411704adc385d9d3c123006c
ilunga josue mulangu <josue.ilunga.mulangu@uqtr.ca>
2025-11-09 21:13:38 -0500

    Ajout de l'attribut disponible et des commentaires

--- Livre.py
+++ Livre.py
@@ -1,7 +1,8 @@
 
 class Livre:
     def __init__(self,id,titre,Auteur ):
-        self.id = id
-        self.titre = titre
-        self.Auteur = Auteur
+        self.id = id # identifiant univique du livre
+        self.titre = titre # titre du livre
+        self.Auteur = [] # Auteur du livre
+        self.disponible = True # Par défaut disponible
 
