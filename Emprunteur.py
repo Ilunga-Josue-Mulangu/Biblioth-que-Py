d64358efc840b56857230065d4aebf1609fdfa2b
ilunga josue mulangu <josue.ilunga.mulangu@uqtr.ca>
2025-11-10 11:55:06 -0500

    Mise a jour dans la classe Emprunteur

--- Emprunteur.py
+++ Emprunteur.py
@@ -1,5 +1,5 @@
 class Emprunteur:
-    def __init__(self,id_emprunteur,nom_emprunteur,livres_empruntees):
-        self.id = id_emprunteur
-        self.nom = nom_emprunteur
-        self.livres_empruntees = livres_empruntees;
+    def __init__(self,id_emprunteur,nom_emprunteur):
+        self.id = id_emprunteur # identifiant univique de l'emprunteur'
+        self.nom = nom_emprunteur # nom de l'emprunteur'
+        self.livres_empruntees = [] # liste des livres empruntes
