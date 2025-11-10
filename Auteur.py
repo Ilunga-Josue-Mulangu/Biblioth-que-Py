200b826de706587345a14348f8c87e3e4a7d080d
ilunga josue mulangu <josue.ilunga.mulangu@uqtr.ca>
2025-11-09 19:59:17 -0500

    Mise à jour dans la classe Auteur (déclaration du constructeur)

---
+++ Auteur.py
@@ -0,0 +1,5 @@
+class Auteur:
+    def __init__(self,nom,nationalite,oeuvres):
+        self.nom = nom # nom de l'auteur'
+        self.nationalite = nationalite # nationalite de l'auteur'
+        self.oeuvres = [] # liste des oeuvres de l'auteur'
