cad12c7fa0df81631456fcc115002e58b842573e
ilunga josue mulangu <josue.ilunga.mulangu@uqtr.ca>
2025-11-10 11:52:39 -0500

    Mise a jour dans la classe Auteur

--- Auteur.py
+++ Auteur.py
@@ -1,5 +1,6 @@
 class Auteur:
-    def __init__(self,nom,nationalite,oeuvres):
+    def __init__(self,nom,nationalite):
         self.nom = nom # nom de l'auteur'
         self.nationalite = nationalite # nationalite de l'auteur'
-        self.oeuvres = [] # liste des oeuvres de l'auteur'
+        self.oeuvres =  [] # liste des oeuvres de l'auteur'
+
