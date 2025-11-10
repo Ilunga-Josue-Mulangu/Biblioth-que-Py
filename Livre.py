e69c0c72f2a6fea199ee2fb3d683e4670017fa9b
ilunga josue mulangu <josue.ilunga.mulangu@uqtr.ca>
2025-11-10 11:51:38 -0500

    Mise a jour dans la classe Livre

--- Livre.py
+++ Livre.py
@@ -1,10 +1,12 @@
 from Auteur import Auteur
 from Emprunteur import Emprunteur
 class Livre:
-    def __init__(self,id_livre,titre,Auteur,Emprunteur ):
-        self.id = id_livre # identifiant univique du livre
+    def __init__(self,id_livre,titre, auteur ):
+        self.id_livre = id_livre # identifiant univique du livre
         self.titre = titre # titre du livre
-        self.Auteur = [] # Auteur du livre
+        self.auteur = auteur # Auteur du livre
         self.disponible = True # Par défaut disponible
-        self.Emprunteur = []
+
+
+
 
