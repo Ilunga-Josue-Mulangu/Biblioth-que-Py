b2c0ea2b63d76154d1d704c108fbca9d91db6e1d
ilunga josue mulangu <josue.ilunga.mulangu@uqtr.ca>
2025-11-10 19:42:59 -0500

    Mise a jour dans la classe bibliotheque

--- Bibliotheque.py
+++ Bibliotheque.py
@@ -1,31 +1,33 @@
 from Livre import Livre
-from Emprunteur import Emprunteur
-from Auteur import Auteur
+
 
 class Bibliotheque :
 
-    def __init__(self):
-        self.Auteur = []
-        self.Livre = []
-        self.Emprunteur = []
+        def __init__(self):
+          self.auteurs = []
+          self.livres= []
+          self.emprunteurs = []
 
         # la methode qui permet d'ajouter un livre
-        def ajouter_livre(id_livre,titre,auteur):
-            self.livres.append(Livre(id_livre,titre,auteur))
-            print ("Livre" +self.Livre + " ajouté à la bibliothèque")
-            return Livre
+        def ajouter_livre(self, id_livre, titre, auteur):
+            nouveau_livre = Livre(id_livre, titre, auteur)
+            self.livres.append(nouveau_livre)
+            print(f"Livre '{titre}' ajouté à la bibliothèque")
+            return nouveau_livre
 
 
-        # la methode qui permet de rechercher un livre
-        def rechercher_livre(titre,auteur):
-            for livre in self.livres:
-                if livre.titre == titre:
-                    if livre.auteur == auteur:
-                     return livre
+            # la methode qui permet de rechercher un livre
+        def rechercher_livre(self,titre, auteur):
+                for livre in self.livres:
+                    if livre.titre == titre:
+                        if livre.auteur == auteur:
+                             return livre
+
+                return None
 
 
         # la methode qui permet d'emprunter un livre'
-        def emprunter_livre(id_livre,id_emprunteur):
+        def emprunter_livre(self,id_livre,id_emprunteur):
             for livre in self.livres:
                 if livre.id == id_livre:
                     if livre.disponible :
@@ -36,18 +38,19 @@ class Bibliotheque :
                     else:
                         print("Le livre" + livre.titre + "  est déjà emprunté. ")
                         return False
-            print ("Livre avec id " + {id_livre} + " non trouvé.")
+            print(f"Livre avec id {id_livre} non trouvé.")
             return False
 
+
+
         # la methode qui permet de retourne un livre
-        def retourner_livre(id_livre , id_emprunteur):
+        def retourner_livre(self,id_livre , id_emprunteur):
 
             livre_Disponible = None
             for livre in self.livres:
                 if livre.id == id_livre:
                     livre_Disponible = livre
                     break
-
 
             if livre_Disponible is None:
                 print (f"Erreur: Livre avec l'id {id_livre} introuvable.")
