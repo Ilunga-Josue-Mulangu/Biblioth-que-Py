4e5e36ba79c3c4c47441747991233a68d5a50730
ilunga josue mulangu <josue.ilunga.mulangu@uqtr.ca>
2025-11-11 20:43:48 -0500

    Mise a jour dans la Main

--- Main.py
+++ Main.py
@@ -8,8 +8,6 @@ ma_biblio = Bibliotheque()
 
 # Créer des auteurs
 
-
-
 auteur1 = Auteur("Victor Hugo", "Française")
 auteur2 = Auteur("Albert Camus", "Française")
 auteur3 = Auteur("Sony Labou Tansi","Congolaise")
@@ -21,20 +19,25 @@ livre1 = ma_biblio.ajouter_livre(1, "Les Misérables", auteur1)
 livre2 = ma_biblio.ajouter_livre(2, "L'Étranger", auteur2)
 livre3 = ma_biblio.ajouter_livre(3, "Notre-Dame de Paris", auteur1)
 livre4 = ma_biblio.ajouter_livre(4,"La vie et demie",auteur3)
-livre5 = ma_biblio.ajouter_livre(3, "Notre-Dame de Paris", auteur1)
+
 
 # Créer des emprunteurs
 
 
-emprunteur1 = Emprunteur(101, "Alice Dupont")
-emprunteur2 = Emprunteur(102, "Bob Martin")
-emprunteur3 = Emprunteur(103, "Charlie Dumont")
+emprunteur1 = Emprunteur(101, " Alice Dupont ")
+emprunteur2 = Emprunteur(102, " Bob Martin ")
+emprunteur3 = Emprunteur(103, " Charlie Dumont ")
+emprunteur4 = Emprunteur(104, " David trump ")
+emprunteur5 = Emprunteur(105,"Jean_Lucien")
+
 
 # Ajouter les emprunteurs à la bibliothèque
 
 ma_biblio.emprunteurs.append(emprunteur1)
 ma_biblio.emprunteurs.append(emprunteur2)
 ma_biblio.emprunteurs.append(emprunteur3)
+ma_biblio.emprunteurs.append(emprunteur4)
+ma_biblio.emprunteurs.append(emprunteur5)
 
 # Afficher les livres disponibles
 print("\n-----------Livres dans la bibliothèque -----------\n")
@@ -43,15 +46,13 @@ for livre in ma_biblio.livres:
 
 # Emprunter un livre
 print("\n------ Emprunts ------\n")
-ma_biblio.emprunter_livre(1, emprunteur1)
-ma_biblio.emprunter_livre(2, emprunteur2)
-ma_biblio.emprunter_livre(1, emprunteur2)
-ma_biblio.emprunter_livre(3, emprunteur3)
 
-# Afficher l'état des livres
-print("\n------État des livres ------\n")
-for livre in ma_biblio.livres:
-    print(livre)
+ma_biblio.emprunter_livre(2, emprunteur2)
+ma_biblio.emprunter_livre(3, emprunteur3)
+ma_biblio.emprunter_livre(4, emprunteur4)
+ma_biblio.emprunter_livre(2, emprunteur5)
+
+
 
 # Afficher les emprunteurs
 print("\n------ Emprunteurs ------\n")
@@ -60,19 +61,19 @@ for emprunteur in ma_biblio.emprunteurs:
 
 # Rechercher un livre
 print("\n------ Recherche ------\n")
-livre_trouve = ma_biblio.rechercher_livre("L'Étranger", auteur2)
+livre_trouve = ma_biblio.rechercher_livre("Les Misérables", auteur1)
 if livre_trouve:
     print(f"Trouvé: {livre_trouve}")
+else:
+    print("Livre non trouvé")
 
 
 
-# Retourner un livre
+# Retourner d'un livre
 print("\n------ Retours ------\n")
 
-
 ma_biblio.retourner_livre(3, 103)
-
-
+ma_biblio.retourner_livre(2, 102)
 
 
 # Afficher l'état final
