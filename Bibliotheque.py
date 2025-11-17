2f47472cecbe833b3d098f78468e874e2da6bacc
ilunga josue mulangu <josue.ilunga.mulangu@uqtr.ca>
2025-11-17 15:02:44 -0500

    Ajout des commentaires

--- Bibliotheque.py
+++ Bibliotheque.py
@@ -54,31 +54,33 @@ class Bibliotheque :
         # la methode qui permet de retourne un livre
         def retourner_livre(self,id_livre , id_emprunteur):
 
+            # permet de rechercher un livre
             livre_Disponible = None
             for livre in self.livres:
                 if livre.id == id_livre:
                     livre_Disponible = livre
                     break
 
+            # permet de vérifier si le livre existe si ce n'est pas le cas on affiche un message d'erreur
             if livre_Disponible is None:
                 print (f"Erreur: Livre avec l'id {id_livre} introuvable.")
                 return False
 
-            #  La methode qui permet de Rechercher l'emprunteur
+            # permet de Rechercher l'emprunteur
             emprunteur_trouve = None
             for emprunteur in self.emprunteurs:
                 if emprunteur.id == id_emprunteur:
                     emprunteur_trouve = emprunteur
                     break
-
+            # permet de vérifier si l'emprunteur existe si ce n'est pas le cas on affiche un message d'erreur
             if emprunteur_trouve is None:
                 print(f"Erreur: Emprunteur avec l'id {id_emprunteur} introuvable.")
                 return False
-
+            # permet de verifier si le livre a été emprunté avant de permettre le retour  du livre si ce n'est pas le cas on affiche un message d'erreur
             if livre_Disponible not in emprunteur_trouve.livres_empruntees:
                 print(f"Erreur :{emprunteur_trouve.nom_emprunteur} n'a pas emprunter le livre {livre_Disponible.titre}.")
                 return False
-
+            # permet d'effectuer le retour d' un livre emprunté
             livre_Disponible.disponible = True
             emprunteur_trouve.livres_empruntees.remove(livre_Disponible)
             print(f"{emprunteur_trouve.nom_emprunteur} a retourné '{livre_Disponible.titre}'")
